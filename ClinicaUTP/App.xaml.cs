875a3dfd6d2bbf20c7475b911e99ebbcef75f1fd
manuelalrod <manuelalmengor32@gmail.com>
2024-09-04 00:17:58 -0500

    Subiendo Proyecto

---
+++ ClinicaUTP/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ClinicaUTP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
